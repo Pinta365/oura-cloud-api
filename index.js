f225d03c901e50633bf571053800972da63e2880
Pinta365 <hello@pinta.land>
2022-09-01 18:27:09 +0200

    update to v2 of the api

--- index.js
+++ index.js
@@ -1,112 +1,86 @@
-'use strict';
+const baseUrl = "https://api.ouraring.com/v2/usercollection/";
 
-const https = require('https');
-const querystring = require('querystring');
-
-class Client {
-
-    constructor(access_token) {
-        if (!access_token) {
-            throw `Missing access token, check documentation.`;
-        }
-        this.access_token = access_token;
-    }
-
-    #call = (url, qs) => {
-
-        var options = {
-            method: 'GET',
-            hostname: 'api.ouraring.com',
-            port: 443,
-            path: '/v1' + encodeURI(url) + (qs ? '?' + querystring.stringify(qs) : ''),
-            headers: {
-                'Authorization': 'Bearer ' + this.access_token
-            },
-        };
-
-        return new Promise((resolve, reject) => {
-            const req = https.request(options, function (res) {
-
-                if (res.statusCode < 200 || res.statusCode >= 300) {
-                    reject(res.statusCode);
-                }
-
-                const parts = [];                
-    
-                res.on('data', function (part) {
-                    parts.push(part);
-                });
-    
-                res.on('end', function () {
-                    try {
-                        const data = JSON.parse(Buffer.concat(parts));
-                        resolve(data); 
-                    } catch (error) {
-                        reject(error);
-                    }
-                       
-                });
-            });
-
-            req.on('error', function (err) {
-                reject(err);
-            });
-    
-            req.end(); 
-
-        });
-    }
-
-    #validateQueryString = (qs, allowedKeys) => {
-        for (var key in qs) {
-            if (qs.hasOwnProperty(key)) {
-                if (allowedKeys.includes(key) === false) {
-                    throw `Invalid parameter '${key}'.`;
-                }
+const validateQS = (qs, allowedKeys) => {
+    for (const key in qs) {
+        if (Object.prototype.hasOwnProperty.call(qs, key)) {
+            if (allowedKeys.includes(key) === false) {
+                throw `Invalid parameter '${key}'.`;
             }
         }
-        return true;
+    }
+    return true;
+};
+
+class Client {
+ 
+    constructor(accessToken) {
+        if (!accessToken) {
+            throw "Missing access token";
+        }
+        this.accessToken = accessToken;
+    }
+ 
+    #runRequest = async (url, qs) => { 
+        const params = new URLSearchParams(qs); 
+
+        const response = await fetch(baseUrl + encodeURI(url) + (qs ? "?" + params.toString() : ""), {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + this.accessToken
+            }
+        });   
+
+        const parsed = await response.json();               
+        if (response.status >= 200 && response.status < 300) {
+            return parsed;
+        } else {
+            throw `${response.status} ${response.statusText}${parsed?.detail ? " - "+JSON.stringify(parsed.detail):""} `;
+        }        
+    };
+ 
+    async getDailyActivity(qs) {
+        validateQS(qs, ["start_date", "end_date", "next_token"]);       
+        return await this.#runRequest("daily_activity", qs);
+    }
+ 
+    async getDailyReadiness(qs) {
+        validateQS(qs, ["start_date", "end_date", "next_token"]);
+        return await this.#runRequest("daily_readiness", qs);  
+    }
+ 
+    async getDailySleep(qs) {
+        validateQS(qs, ["start_date", "end_date", "next_token"]);  
+        return await this.#runRequest("daily_sleep", qs);  
+    }
+ 
+    async GetHeartrate(qs) {
+        validateQS(qs, ["start_datetime", "end_datetime", "next_token"]);  
+        return await this.#runRequest("heartrate", qs);  
+    }
+ 
+    async GetPersonalInfo() {
+        return await this.#runRequest("personal_info");
+    }
+ 
+    async getSession(qs) {
+        validateQS(qs, ["start_date", "end_date", "next_token"]);  
+        return await this.#runRequest("session", qs);
     }
 
-    async getUserInfo() {
-        return await this.#call('/userinfo');
+    async getSleep(qs) {
+        validateQS(qs, ["start_date", "end_date", "next_token"]);  
+        return await this.#runRequest("sleep", qs);  
     }
 
-    getPersonalInfo() {
-        // Alias for getUserInfo.
-        return this.getUserInfo();
+    async getTag(qs) {
+        validateQS(qs, ["start_date", "end_date", "next_token"]);
+        return await this.#runRequest("tag", qs);    
     }
 
-    async getSleepSummaries(qs) {
-        this.#validateQueryString(qs, ['start', 'end']);
-        const call = await this.#call('/sleep', qs);
-        return call.sleep;
-    }
-
-    async getActivitySummaries(qs) {
-        this.#validateQueryString(qs, ['start', 'end']);
-        const call = await this.#call('/activity', qs);
-        return call.activity;
-    }
-
-    async getReadinessSummaries(qs) {
-        this.#validateQueryString(qs, ['start', 'end']);
-        const call = await this.#call('/readiness', qs);
-        return call.readiness;
-    }
-
-    async getSummaries(qs) {
-        const sleep = await this.#call('/sleep', qs);
-        const activity = await this.#call('/activity', qs);
-        const readiness = await this.#call('/readiness', qs);
-        return { sleep: sleep.sleep, activity: activity.activity, readiness: readiness.readiness };
-    }
-
-    async getIdealBedtime(qs) {
-        this.#validateQueryString(qs, ['start', 'end']);
-        const call = await this.#call('/bedtime', qs);
-        return call.ideal_bedtimes;
+    async getWorkout(qs) {
+        validateQS(qs, ["start_date", "end_date", "next_token"]);
+        return await this.#runRequest("workout", qs);  
     }
 }
 
-module.exports = Client; 
+module.exports = Client;
